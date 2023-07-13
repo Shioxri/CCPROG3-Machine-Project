e0e9292fbc2a9551244b7e21b7bd5e6430ffd9dd
Shioxri <troybelardo@gmail.com>
2023-07-13 21:11:27 +0800

    added slot and item class

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class VendingMachine {
+
+    private ArrayList<Slot> slotArrayList;
+
+
+    public VendingMachine() {
+        slotArrayList = new ArrayList<>(8);
+    }
+
 }
