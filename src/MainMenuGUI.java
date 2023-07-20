131442f7175afbf48ff8cf0a965388e716b9dc35
karlOrdinario <karl_ordinario@dlsu.edu.ph>
2023-07-20 18:01:13 +0800

    Added Dimensions, color, icon, and title.

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -1,12 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainMenuGUI {
 
-    JFrame frame = new JFrame();
-    JLabel label = new JLabel();
-    JPanel mainPanel = new JPanel();
+    public static void main(String[] Args) {
 
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel();
+        JPanel mainPanel = new JPanel();
 
+        ImageIcon fruitIcon = new ImageIcon("323528583_464396995737157_7527402304455667779_n.png");
+
+        frame.setTitle("Vending Machine");
+        frame.setIconImage(fruitIcon.getImage());
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(36,37,38));
+        label.setText("Vending Machine Main Menu");
+        frame.add(label);
+    }
 
 
 }
