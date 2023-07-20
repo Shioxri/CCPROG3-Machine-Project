753ea742b4c3899c7c393df837e8da17c6dcc546
karlOrdinario <karl_ordinario@dlsu.edu.ph>
2023-07-20 20:26:42 +0800

    added gray Title panel

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -6,28 +6,35 @@ public class MainMenuGUI {
     public static void main(String[] Args) { // this is temporary and for testing only, move to driver
 
         JFrame frame = new JFrame();
-        JLabel label = new JLabel();
-        JPanel mainPanel = new JPanel();
+        JLabel titleLabel = new JLabel();
+        JPanel titlePanel = new JPanel();
 
         ImageIcon fruitIcon = new ImageIcon("pixelatedfruit.png"); // image
 
-        label.setText("Vending Machine Main Menu");// adds label of text
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.BOTTOM);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setForeground(Color.white);// font color for label
-        label.setFont(new Font("Century Gothic", Font.BOLD,20)); // sets font style and size for the label
-        frame.add(label);// adds the label to the frame
+        titleLabel.setText("Vending Machine Main Menu");// adds label of text
+        /*
+        titleLabel.setHorizontalTextPosition(JLabel.CENTER);
+        titleLabel.setVerticalTextPosition(JLabel.BOTTOM);
+         */
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setVerticalAlignment(JLabel.TOP);
+        titleLabel.setForeground(Color.white);// font color for label
+        titleLabel.setFont(new Font("Century Gothic", Font.BOLD,10)); // sets font style and size for the label
+
+
+        titlePanel.setBackground(Color.gray);
+        titlePanel.setBounds(100,0,500,250);
+        titlePanel.setLayout(null);
 
         frame.setTitle("Vending Machine"); // frame title
         frame.setIconImage(fruitIcon.getImage()); // sets the icon image
-        frame.setResizable(true);// prevents resizing
+        frame.setResizable(false);// prevents resizing
         frame.setVisible(true);// sets the frame to be visible
-        frame.setSize(500,500);// size or dimensions of the frame
+        frame.setSize(750,750);// size or dimensions of the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// makes pressing x stop the program
         frame.getContentPane().setBackground(new Color(36,37,38));// sets the background color
-
+        frame.setLayout(null);
+        frame.add(titlePanel);
     }
 
 
