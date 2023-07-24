5ff835ffb80b0f7cefb756d027a5ee7438fb9663
karlOrdinario <karl_ordinario@dlsu.edu.ph>
2023-07-24 19:12:30 +0800

    New additions/edits:
    - Added Buttons to the selection panel

--- src/View/RegularBuy.java
+++ src/View/RegularBuy.java
@@ -17,9 +17,15 @@ public class RegularBuy {
         JPanel selectionPanel = new JPanel();
         JPanel lowerPanel = new JPanel();
         JPanel rightPanel = new JPanel();
-        JButton UseVMButton = new JButton();
-        JButton maintenanceButton = new JButton();
-        JButton backButton = new JButton();
+        JButton Item1Button = new JButton();
+        JButton Item2Button = new JButton();
+        JButton Item3Button = new JButton();
+        JButton Item4Button = new JButton();
+        JButton Item5Button = new JButton();
+        JButton Item6Button = new JButton();
+        JButton Item7Button = new JButton();
+        JButton Item8Button = new JButton();
+        JButton Item9Button = new JButton();
 
 
         String musicFilePath = "music.wav"; // Make sure the music.wav file is in the same directory as the source file
@@ -44,24 +50,48 @@ public class RegularBuy {
         bgpanel.add(backgroundLabel);
 
         // Buttons
-        UseVMButton.setBounds(100, 75, 300, 50);
-        UseVMButton.setHorizontalAlignment(JButton.CENTER);
-        UseVMButton.setIcon(cola);
-        UseVMButton.setText("Use Vending Machine");
-        UseVMButton.setHorizontalTextPosition(JButton.CENTER);
-        UseVMButton.addActionListener(e -> System.out.println("show regular VM"));
+        Item1Button.setBounds(25, 75, 150, 50);
+        Item1Button.setHorizontalAlignment(JButton.CENTER);
+        Item1Button.setIcon(cola);
+        Item1Button.setText("Item 1");
+        Item1Button.setHorizontalTextPosition(JButton.CENTER);
+        Item1Button.addActionListener(e -> System.out.println("show regular VM"));
 
-        maintenanceButton.setBounds(100, 175, 300, 50);
-        maintenanceButton.setHorizontalAlignment(JButton.CENTER);
-        maintenanceButton.setText("Enter Maintenance Mode");
-        maintenanceButton.setHorizontalTextPosition(JButton.CENTER);
-        maintenanceButton.setIcon(sampleIcon);
-        maintenanceButton.addActionListener(e -> System.out.println("show special VM"));
+        Item2Button.setBounds(200, 75, 150, 50);
+        Item2Button.setHorizontalAlignment(JButton.CENTER);
+        Item2Button.setText("Item 2");
+        Item2Button.setHorizontalTextPosition(JButton.CENTER);
+        Item2Button.setIcon(sampleIcon);
+        Item2Button.addActionListener(e -> System.out.println("show special VM"));
 
-        backButton.setBounds(100, 275, 300, 50);
-        backButton.setHorizontalAlignment(JButton.CENTER);
-        backButton.setText("Back");
-        backButton.addActionListener(e -> System.exit(0));
+        Item3Button.setBounds(375, 75, 150, 50);
+        Item3Button.setHorizontalAlignment(JButton.CENTER);
+        Item3Button.setText("Item 3");
+        Item3Button.addActionListener(e -> System.exit(0));
+
+        Item4Button.setBounds(25, 175, 150, 50);
+        Item4Button.setHorizontalAlignment(JButton.CENTER);
+        Item4Button.setText("Item 4");
+
+        Item5Button.setBounds(200, 175, 150, 50);
+        Item5Button.setHorizontalAlignment(JButton.CENTER);
+        Item5Button.setText("Item 5");
+
+        Item6Button.setBounds(375, 175, 150, 50);
+        Item6Button.setHorizontalAlignment(JButton.CENTER);
+        Item6Button.setText("Item 6");
+
+        Item7Button.setBounds(25, 275, 150, 50);
+        Item7Button.setHorizontalAlignment(JButton.CENTER);
+        Item7Button.setText("Item 7");
+
+        Item8Button.setBounds(200, 275, 150, 50);
+        Item8Button.setHorizontalAlignment(JButton.CENTER);
+        Item8Button.setText("Item 8");
+
+        Item9Button.setBounds(375, 275, 150, 50);
+        Item9Button.setHorizontalAlignment(JButton.CENTER);
+        Item9Button.setText("Not Available");
 
         // Label
 
@@ -84,9 +114,15 @@ public class RegularBuy {
         selectionPanel.setBounds(0, 150, 550, 480);
         selectionPanel.setLayout(null);
         selectionPanel.setOpaque(true);
-        selectionPanel.add(UseVMButton);
-        selectionPanel.add(maintenanceButton);
-        selectionPanel.add(backButton);
+        selectionPanel.add(Item1Button);
+        selectionPanel.add(Item2Button);
+        selectionPanel.add(Item3Button);
+        selectionPanel.add(Item4Button);
+        selectionPanel.add(Item5Button);
+        selectionPanel.add(Item6Button);
+        selectionPanel.add(Item7Button);
+        selectionPanel.add(Item8Button);
+        selectionPanel.add(Item9Button);
         selectionPanel.setBorder(borderLine);
 
         lowerPanel.setBackground(new Color(25, 25, 112, 123));
