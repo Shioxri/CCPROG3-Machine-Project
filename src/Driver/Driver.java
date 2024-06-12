9df8848350144187210da42e0364ebc84122bc8d
Shioxri <troybelardo@gmail.com>
2024-06-12 13:50:14 +0800

    moved assets to src, added commit and run instructions

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -3,14 +3,20 @@ package Driver;/*
         Karl Andrei L. Ordinario - 12209732
         S18A | Aug. 3, 2023
 
+        To run and compile:
+        javac Model\Initializer.java View\MainMenuGUI.java Controller\MainMenuController.java Driver\Driver.java
+        java Driver\Driver.java
+
  */
 import Controller.MainMenuController;
 import View.MainMenuGUI;
 
 public class Driver {
+
     public static void main(String[] args) {
         // Create and show the main menu GUI
         MainMenuGUI mainMenuGUI = new MainMenuGUI();
         mainMenuGUI.getFrame().setVisible(true);
         MainMenuController mainMenuController = new MainMenuController(mainMenuGUI);
-    }}
+    }
+}
